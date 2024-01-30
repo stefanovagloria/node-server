bf9db3a0bfafcb81d4d7fdd91c80fc312a30257e
stefanovagloria <gloriqstefanova97@gmail.com>
2024-01-30 07:24:55 +0300

    middlewares added and routes configured

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000);
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
+app.use((req,res,next) =>{
+    res.status(404).send('<h1>Page Not Found</h1>');
+})
+
+
+app.listen(3000);
